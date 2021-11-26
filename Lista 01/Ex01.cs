01e35f79e690e6252610e98361e4f56ecd1585d0
Leonardo Nadson <72714982+leonardonadson@users.noreply.github.com>
2021-11-26 12:22:39 -0300

    Delete Lista 01 directory

--- Lista 01/Ex01.cs
+++
@@ -1,8 +0,0 @@
-//Objetivo: Mostrar uma mensagem na tela com o seu nome.
-using System;
-
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Leonardo Nadson Oliveira de Medeiros");
-  }
-}
--- Lista 01/Ex02.cs
+++
@@ -1,8 +0,0 @@
-//Objetivo: Mostrar uma mensagem na tela com a sua matrícula.
-using System;
-
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("20211011110036");
-  }
-}
